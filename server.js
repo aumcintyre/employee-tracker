a0911a8a103b0bdc8c891beac286c2ea18f00fee
aumcintyre <aumcintyre@gmail.com>
2022-10-04 13:09:21 -0500

    added dependencies and built initial prompt

---
+++ server.js
@@ -0,0 +1,15 @@
+const inquirer = require('inquirer')
+const mysql2 = require('mysql2')
+
+const runPrompt = () => {
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'choice',
+            message: 'What would you like to do?',
+            choices: ['View All Employees', 'Add Employee', 'Update Employee Role', 'View All Roles', 'Add Role', 'View All Departments', 'Add Department']
+        }
+    ])
+}
+
+runPrompt()
