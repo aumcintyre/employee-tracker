4934b0fbf7787e16f03a69874383194d92b3b0ac
aumcintyre <aumcintyre@gmail.com>
2022-10-05 17:54:37 -0500

    added if statements to prompt response

--- server.js
+++ server.js
@@ -10,7 +10,44 @@ const runPrompt = () => {
             choices: ['View All Employees', 'Add Employee', 'Update Employee Role', 'View All Roles', 'Add Role', 'View All Departments', 'Add Department']
         }
     ])
+        .then((response) => {
+
+//WHY ISN'T THIS LINKING???
+
+
+            if (response === 'View All Employees') {
+                showEmployees();
+            }
+
+            if (choices === 'Add Employee') {
+                console.log('Adding new employee');
+            }
+
+            if (choices === 'Update Employee Roles') {
+                console.log('Updating employee role');
+            }
+
+            if (choices === 'View All Roles') {
+                console.log('Showing all roles');
+            }
+
+            if (choices === 'Add Role') {
+                console.log('Adding new role');
+            }
+
+            if (choices === 'View All Departments') {
+                console.log('Showing all departments');
+            }
+
+            if (choices === 'Add Department') {
+                console.log('Adding new department');
+            }
+        })
 }
 
 console.log("Welcome to the Employee Tracker!");
 runPrompt()
+
+showEmployees = () => {
+    console.log('Showing all employees');
+}
