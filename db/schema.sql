e844452ca492918d0d6ec72f654fd8a535f8ea02
aumcintyre <aumcintyre@gmail.com>
2022-10-12 10:30:26 -0500

    schema and seeds working, tables populated

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,34 @@
+-- Active: 1664493076393@@127.0.0.1@3306@employee_db
+
 DROP DATABASE IF EXISTS employee_db;
 
-CREATE DATABASE
+CREATE DATABASE employee_db;
+
+USE employee_db;
+
+CREATE TABLE
+    department(
+        id INTEGER PRIMARY KEY NOT NULL AUTO_INCREMENT,
+        name VARCHAR(25) NOT NULL
+    );
+
+CREATE TABLE
+    role(
+        id INTEGER PRIMARY KEY AUTO_INCREMENT,
+        title VARCHAR(25) NOT NULL,
+        salary DECIMAL NOT NULL,
+        department_id INTEGER,
+        FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE
+        SET NULL
+    );
+
+CREATE Table
+    employee (
+        id INTEGER PRIMARY KEY AUTO_INCREMENT,
+        first_name VARCHAR(25) NOT NULL,
+        last_name VARCHAR(25) NOT NULL,
+        role_id INTEGER,
+        FOREIGN KEY (role_id) REFERENCES role(id),
+        manager_id INTEGER,
+        FOREIGN KEY (manager_id) REFERENCES employee(id)
+    )
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,1 +1,31 @@
-INSERT INTO 
+-- Active: 1664493076393@@127.0.0.1@3306@employee_db
+
+INSERT INTO department (name)
+VALUES 
+('Marketing'),
+('Information Technology'),
+('Finance'),
+('Accounting'),
+('R & D');
+
+INSERT INTO role (title, salary, department_id)
+VALUES
+('Software Engineer', 120000, 1),
+('Full Stack Developer', 95000, 1),
+('Accountant', 120000, 2), 
+('Finanical Analyst', 150000, 2),
+('Brand Ambassador', 35000, 3), 
+('Project Manager', 100000, 4);
+
+
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES 
+('Austin', 'McIntyre', 2, null),
+('Brian', 'Mills', 1, 1),
+('Petunia', 'Gray', 4, null),
+('Ashley', 'Flowers', 3, 3),
+('James', 'Mack', 6, null),
+('Sara', 'Cardon', 5, 5),
+('Luis', 'Lopez', 5, null),
+('Graciela', 'Chavez', 4, null);
